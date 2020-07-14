bd7ed94b8bc9f919f47af11742f6c09ed80c8d59
Roman <romanjeanelie@yahoo.fr>
2020-07-14 16:58:58 +0200

    changes Patrick

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello je m'appelle Roman</p>
+        <p>Ah non pardon, moi c'est Patrick</p>
         <a
           className="App-link"
           href="https://reactjs.org"
